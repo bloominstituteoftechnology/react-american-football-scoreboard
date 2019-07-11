8802efbad69d8dd9df9df88df401aa2ff4072075
vy3191 <venkyafhs@gmail.com>
2019-07-11 11:33:19 -0400

    Imported userState hook and established application's state with userState hooks

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 //TODO: STEP 1 - Import the useState hook.
-import React from "react";
+import React, {userState} from "react";
 import "./App.css";
 import BottomRow from "./BottomRow";
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
+  const [homeScore, setHomeScore] = userState();
+  const [awayScore, setAwayScore] = userState()
 
   return (
     <div className="container">
@@ -11,7 +13,6 @@ function App() {
         <div className="topRow">
           <div className="home">
             <h2 className="home__name">Lions</h2>
-
             {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
 
             <div className="home__score">32</div>
