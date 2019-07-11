71e18588701b5ffa0bf602707c2ea6eecd425d95
vy3191 <venkyafhs@gmail.com>
2019-07-11 11:26:56 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@
 import React from "react";
 import "./App.css";
 import BottomRow from "./BottomRow";
-
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
 
