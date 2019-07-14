66b259dd5c04c94332f64e15ce862855d0b2c686
meganwhite <mfw247@gmail.com>
2019-07-14 21:40:00 +0800

    initialized score values using hooks

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import BottomRow from "./BottomRow";
 
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
-
+  const [lionsScore, setLionsScore] = useState(0); // Give these better names, and decide whether you want to pass an initial score into the state hook as the initialValue
+  const [tigersScore, setTigersScore] = useState(0); 
   return (
     <div className="container">
       <section className="scoreboard">
@@ -15,12 +16,12 @@ function App() {
 
             {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
 
-            <div className="home__score">32</div>
+            <div className="home__score">{lionsScore}</div>
           </div>
           <div className="timer">00:03</div>
           <div className="away">
             <h2 className="away__name">Tigers</h2>
-            <div className="away__score">32</div>
+            <div className="away__score">{tigersScore}</div>
           </div>
         </div>
         <BottomRow />
