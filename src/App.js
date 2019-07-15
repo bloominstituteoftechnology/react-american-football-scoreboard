cf6d3753a46cb2ae3fbd9235c52753d2ee4db86b
BNMoyers <50836425+BNMoyers@users.noreply.github.com>
2019-07-15 17:32:31 -0500

    stretch goals 1 + 3 complete

--- src/App.js
+++ src/App.js
@@ -2,22 +2,27 @@
 import React, {useState} from "react";
 import "./App.css";
 import Scoreboard from "./scoreboard";
-import Buttons from "./Buttons";
+
+
 
 
 
 
 function App() {
-  //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
-  const [homeScore, setHomeScore] = useState(0);
-  const [awayScore, setAwayScore] = useState(0);
+  //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score. ---these were moved to the appropriate files; commented out for reference.
+  // const [homeScore, setHomeScore] = useState(0);
+  // const [awayScore, setAwayScore] = useState(0);
   return (
     <div className="container">
       
         <Scoreboard />
-     <Buttons />
+     
     </div>
   );
 }
+
+
+
+
 
 export default App;
--- src/Buttons.js
+++ src/Buttons.js
@@ -2,24 +2,23 @@ import React, {useState} from "react";
 import "./App.css";
 
 
-const Buttons = () => {
-    const [homeScore,setHomeScore] = useState(0);
-    const [awayScore,setAwayScore] = useState(0);
+
+const Buttons = (props) => {
+
 
     return(
         <section className="buttons">
         <div className="homeButtons">
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button className="homeButtons__touchdown" onClick={() => setHomeScore(homeScore + 10)}>Gryffindor Scores!</button>
-          <button className="homeButtons__fieldGoal" onClick={() => setHomeScore(homeScore + 150)}>Gryffindor Catches the Snitch!</button>
+          <button className="homeButtons__goal" onClick={() => props.setHomeScore(props.homeScore + 10)}>Gryffindor Scores!</button>
+          <button className="homeButtons__snitch" onClick={() => props.setHomeScore(props.homeScore + 150)}>Gryffindor Catches the Snitch!</button>
         </div>
         <div className="awayButtons">
-        <button className="awayButtons__touchdown" onClick={() => setAwayScore(awayScore + 10)}>Slytherin Scores!</button>
-          <button className="awayButtons__fieldGoal" onClick={() => setAwayScore(awayScore + 150)}>Slytherin Catches the Snitch!</button>
+        <button className="awayButtons__touchdown" onClick={() => props.setAwayScore(props.awayScore + 10)}>Slytherin Scores!</button>
+          <button className="awayButtons__fieldGoal" onClick={() => props.setAwayScore(props.awayScore + 150)}>Slytherin Catches the Snitch!</button>
         </div>
       </section>
      
     )
-}
-
+  }
 export default Buttons
