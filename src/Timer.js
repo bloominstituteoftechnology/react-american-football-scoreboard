ba99d940560937c3159aae44839cd94751e4e103
Greg Johnston <GCJohnston91@gmail.com>
2019-07-15 16:23:24 -0400

    almost done with timer

--- src/Timer.js
+++ src/Timer.js
@@ -3,19 +3,28 @@ import ReactDOM from "react-dom";
 
 function Timer () {
 
-  const [secondsElapsed] = useState(50);
+  const [secondsElapsed] = useState(65);
   console.log(secondsElapsed);
   const getSeconds = function () {
     return (`0${secondsElapsed % 60}`).slice(-2);
   }
   console.log(getSeconds);
+  const getMinutes = function () {
+    return Math.floor(secondsElapsed / 60);
+  }
 
   const startGame = () =>
     alert('Starting game');
+    setInterval(function () {
+      setState({
+        secondsElapsed: secondsElapsed +1
+      })
+    }, 1000)
+
 
     return (
       <div className="timer">
-        <p className ="timer"> { getSeconds() } </p>
+        <p className ="timer"> {getMinutes()}:{ getSeconds() } </p>
         <button type="button" onClick={ () => startGame()}>Start Game</button>
       </div>
     );
