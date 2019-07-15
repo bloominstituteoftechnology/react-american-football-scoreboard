e09d45bc07cbdb42658c6c6045b61a7eb8483b99
pj-wise <51010291+pj-wise@users.noreply.github.com>
2019-07-15 17:03:22 -0400

    switched out hard coded data within divs to interpolated { } data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 //TODO: STEP 1 - Import the useState hook.
-import React, { useState } from "react";
+import React, { useState } from "react"; //import useState
 import "./App.css";
 import BottomRow from "./BottomRow";
 
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
+
   //home
   const [homeTeam, setHome] = useState("Lions");
   const [homeScore, setHomeScore] = useState(0);
@@ -12,20 +13,23 @@ function App() {
   const [awayTeam, setAway] = useState("Tigers");
   const [awayScore, setAwayScore] = useState(0);
 
+  //timer
+  const [timer, setTimer] = useState("30:00");
+
   return (
     <div className="container">
       <section className="scoreboard">
         <div className="topRow">
           <div className="home">
-            <h2 className="home__name">Lions</h2>
+            <h2 className="home__name">{homeTeam}</h2>
 
             {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
 
-            <div className="home__score">32</div>
+            <div className="home__score">{homeScore}</div>
           </div>
-          <div className="timer">00:03</div>
+          <div className="timer">{timer}</div>
           <div className="away">
-            <h2 className="away__name">Tigers</h2>
+            <h2 className="away__name">{awayTeam}</h2>
             <div className="away__score">{awayScore}</div>
           </div>
         </div>
