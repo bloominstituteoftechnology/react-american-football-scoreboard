7aee3678eba9e41bc60f746c7dc147d49942983f
dangolbeeker <scottb305@outlook.com>
2019-07-15 14:25:06 -0400

    added click liseners MVP

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 //TODO: STEP 1 - Import the useState hook.
-import React from "react";
 import React, { useState } from "react";   //imported useState 
 import "./App.css";
 import BottomRow from "./BottomRow";
@@ -31,12 +30,12 @@ function App() {
       <section className="buttons">
         <div className="homeButtons">
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button className="homeButtons__touchdown">Home Touchdown</button>
-          <button className="homeButtons__fieldGoal">Home Field Goal</button>
+          <button onClick={() => setLionsScore(lionsScore + 7)} className="homeButtons__touchdown">Home Touchdown</button>
+          <button onClick={() => setLionsScore(lionsScore + 3)} className="homeButtons__fieldGoal">Home Field Goal</button>
         </div>
         <div className="awayButtons">
-          <button className="awayButtons__touchdown">Away Touchdown</button>
-          <button className="awayButtons__fieldGoal">Away Field Goal</button>
+        <button onClick={() => setTigersScore(tigersScore + 7)} className="awayButtons__touchdown">Away Touchdown</button>
+          <button onClick={() => setTigersScore(tigersScore + 3)} className="awayButtons__fieldGoal">Away Field Goal</button>
         </div>
       </section>
     </div>
