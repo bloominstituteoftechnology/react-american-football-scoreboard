d7af4a28fd36f834d89841dc853c413e62a48ddb
Reese Kunz <reesekunz21@gmail.com>
2019-07-15 13:21:54 -0700

    added click listeners for each score outcome

--- src/App.js
+++ src/App.js
@@ -37,15 +37,23 @@ function App() {
           className="homeButtons__touchdown">Home Touchdown</button>
 
           <button 
+          onClick = {()=>setHomeScore(homeScore + 3)
           
+          }
           className="homeButtons__fieldGoal">Home Field Goal</button>
         </div>
         <div className="awayButtons">
           <button 
+           onClick = {()=>setAwayScore(awayScore + 7)
+          
+           }
           
           className="awayButtons__touchdown">Away Touchdown</button>
           <button 
+          onClick = {()=>setAwayScore(awayScore + 3)
           
+          }
+         
           className="awayButtons__fieldGoal">Away Field Goal</button>
         </div>
       </section>
