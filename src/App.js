977bc8b844b4264258f5a818f5687d49413bdab9
Greg Johnston <GCJohnston91@gmail.com>
2019-07-15 14:21:59 -0400

    cleaned up functions. researching stretch goals

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
   const [ homeScore, setHomeScore ] = useState(32);
   const [ awayScore, setAwayScore ] = useState(32);
+  console.log('homescore', homeScore);
+  console.log('sethomescore', setHomeScore);
 
   return (
     <div className="container">
@@ -30,12 +32,13 @@ function App() {
       <section className="buttons">
         <div className="homeButtons">
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button className="homeButtons__touchdown" onClick={() => setHomeScore(homeScore + 7)}>>Home Touchdown</button>
-          <button className="homeButtons__fieldGoal" onClick={() => setHomeScore(homeScore + 3)}>>Home Field Goal</button>
+          <button className="homeButtons__touchdown" onClick={ () => setHomeScore(homeScore + 7)}> Home Touchdown</button>
+          <button className="homeButtons__fieldGoal" onClick={ () => setHomeScore(homeScore + 3)}> Home Field Goal</button>
         </div>
+        // Hey Scott
         <div className="awayButtons">
-          <button className="awayButtons__touchdown">Away Touchdown</button>
-          <button className="awayButtons__fieldGoal">Away Field Goal</button>
+          <button className="awayButtons__touchdown" onClick={ () => setAwayScore(awayScore + 7) } >Away Touchdown</button>
+          <button className="awayButtons__fieldGoal" onClick={ () => setAwayScore(awayScore + 3) } >Away Field Goal</button>
         </div>
       </section>
     </div>
