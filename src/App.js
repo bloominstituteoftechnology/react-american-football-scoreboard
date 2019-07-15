541a7c7a0c0dd203840bf906217b20820e919beb
yurika sakae <yurika.sun@gmail.com>
2019-07-15 14:02:19 -0700

    away scores functionality added MVP complete

--- src/App.js
+++ src/App.js
@@ -45,8 +45,18 @@ function App() {
           </button>
         </div>
         <div className="awayButtons">
-          <button className="awayButtons__touchdown">Away Touchdown</button>
-          <button className="awayButtons__fieldGoal">Away Field Goal</button>
+          <button
+            onClick={() => setAwayScore(awayScore + 7)}
+            className="awayButtons__touchdown"
+          >
+            Away Touchdown
+          </button>
+          <button
+            onClick={() => setAwayScore(awayScore + 3)}
+            className="awayButtons__fieldGoal"
+          >
+            Away Field Goal
+          </button>
         </div>
       </section>
     </div>
