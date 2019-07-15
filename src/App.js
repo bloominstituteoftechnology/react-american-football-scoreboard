2bdf958c69b4de111acc816fe3620be695105780
yurika sakae <yurika.sun@gmail.com>
2019-07-15 13:54:57 -0700

    homeTouchDwon added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 //TODO: STEP 1 - Import the useState hook.
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import BottomRow from "./BottomRow";
+import { render } from "react-dom";
 
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
+  const [homeScore, setHomeScore] = useState(0);
+  const [awayScore, setAwayScore] = useState(0);
 
   return (
     <div className="container">
@@ -15,7 +18,7 @@ function App() {
 
             {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
 
-            <div className="home__score">32</div>
+            <div className="home__score">{homeScore}</div>
           </div>
           <div className="timer">00:03</div>
           <div className="away">
@@ -28,7 +31,12 @@ function App() {
       <section className="buttons">
         <div className="homeButtons">
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button className="homeButtons__touchdown">Home Touchdown</button>
+          <button
+            onClick={() => setHomeScore(homeScore + 7)}
+            className="homeButtons__touchdown"
+          >
+            Home Touchdown
+          </button>
           <button className="homeButtons__fieldGoal">Home Field Goal</button>
         </div>
         <div className="awayButtons">
