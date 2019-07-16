53d9a5f3fb84697b5aeb454c84458f39b3a156a4
ebisLab <ebaiden@techie.com>
2019-07-15 21:19:20 -0400

    rewrote code so now the click handler displays the score onclick

--- src/App.js
+++ src/App.js
@@ -10,8 +10,8 @@ function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  
   //You'll need one for the home score and another for the away score.
 
-  const [home, setHome] = useState(false);
-  const [away, setAway] = useState(false);
+  const [home, setHome] = useState(0)
+  const [away, setAway] = useState(0)
 
   return (
    <div className = 'App'>
@@ -36,20 +36,20 @@ function App() {
       <section className="buttons">
         <div className="homeButtons">
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button className="homeButtons__touchdown App" onClick={useState(false)}>Home Touchdown</button>
+          <button className="homeButtons__touchdown" onClick={() => setHome(home+7)}>Home Touchdown</button>
           
-          <button className="homeButtons__fieldGoal App" onClick={useState(false)}>Home Field Goal</button>
+          <button className="homeButtons__fieldGoal" onClick={() => setHome(home+3)}>Home Field Goal</button>
         </div>
         <div className="awayButtons">
-          <button className="awayButtons__touchdown App" onClick={useState(false)}>Away Touchdown</button>
-          <button className="awayButtons__fieldGoal App" onClick={useState(false)}>Away Field Goal</button>
+          <button className="awayButtons__touchdown App" onClick={() => setAway(away+7)}>Away Touchdown</button>
+          <button className="awayButtons__fieldGoal App" onClick={() => setAway(away+3)}>Away Field Goal</button>
         </div>
       </section>
     </div>
     </div>
   );
 }
-const rootElement = document.getElementById("root");
-render(<App />, rootElement);
+// const rootElement = document.getElementById("root");
+// render(<App />, rootElement);
 
 export default App;
