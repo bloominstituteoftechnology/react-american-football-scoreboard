6fda590098b086100596c1e6eadddf015317ae90
Sean Doyle <sdoylelambda@gmail.com>
2019-07-16 13:28:21 -0400

    Quarter resets after 4.

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -13,13 +13,24 @@ return (
             <button onClick={() => useState.setAway(useState.awayScore + 7)}  className="awayButtons__touchdown">Away Touchdown</button>
             <button onClick={() => useState.setAway(useState.awayScore + 3)}  className="awayButtons__fieldGoal">Away Field Goal</button>
         </div>
-        <div className="quarterButtons">
-            <button onClick={() => useState.setQuarter(useState.quarter + 1)}  className="awayButtons__quarter">Quarter</button>
+        <div className="awayButtons__quarter">
+            {/* <button onClick={() =>  useState.setQuarter(useState.quarter + 1)}  className="awayButtons__quarter">Quarter</button> */}
+
+            <button onClick={() =>   useState.setQuarter(useState.quarter + 1)}  
+            className="awayButtons__quarter">
+            {useState.quarter === 5 ? useState.setQuarter(useState.quarter === 0) : useState.quarter + 2 }Quarter</button>
+            
+            
+
+
+
+
+
         </div>
-        <div className="timerButtons">
+        <div className="awayButtons__timerButtons">
             {/* <button onClick={() => useState.setTimer(useState.timer)}>Timer</button> */}
             <button onClick={() => useState.setTimer(!useState.pause)} className="App">
-             {useState.pause === false ? useState.play : useState.pause}>Timer</button>
+             {useState.pause === false ? useState.play : useState.pause}Timer</button>
         </div>
     </section>
     )
