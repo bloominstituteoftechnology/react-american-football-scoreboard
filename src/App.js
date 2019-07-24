0cc00b48f20c040c15c3f5777069a94a48ae556b
Ricardo Barbosa <ric3639@gmail.com>
2019-07-24 12:16:57 -0700

    Field goal value changed to 3 points

--- src/App.js
+++ src/App.js
@@ -32,11 +32,11 @@ function App() {
         <div className="homeButtons">
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
           <button onClick = { () => homeSetScore(homeScore+7) } className="homeButtons__touchdown">Home Touchdown</button>
-          <button onClick = { () => homeSetScore(homeScore +1)} className="homeButtons__fieldGoal">Home Field Goal</button>
+          <button onClick = { () => homeSetScore(homeScore +3)} className="homeButtons__fieldGoal">Home Field Goal</button>
         </div>
         <div className="awayButtons">
           <button onClick = { () => awaySetScore(awayScore+7) } className="awayButtons__touchdown">Away Touchdown</button>
-          <button onClick = { () => awaySetScore(awayScore+1) } className="awayButtons__fieldGoal">Away Field Goal</button>
+          <button onClick = { () => awaySetScore(awayScore+3) } className="awayButtons__fieldGoal">Away Field Goal</button>
         </div>
       </section>
     </div>
