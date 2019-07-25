51ea4b24b0e5b29ea6d2e29e51ed1c4612d4814f
Jashele T <jashele@Jashele.domain>
2019-07-24 19:30:07 -0700

    fixed points systems numbers

--- src/App.js
+++ src/App.js
@@ -31,12 +31,12 @@ function App() {
       <section className="buttons">
         <div className="homeButtons">
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button onClick={() => initialValue1(homeCount + 3)} className="homeButtons__touchdown">Home Touchdown</button>
-          <button onClick={() => initialValue1(homeCount + 7)} className="homeButtons__fieldGoal">Home Field Goal</button>
+          <button onClick={() => initialValue1(homeCount + 7)} className="homeButtons__touchdown">Home Touchdown</button>
+          <button onClick={() => initialValue1(homeCount + 3)} className="homeButtons__fieldGoal">Home Field Goal</button>
         </div>
         <div className="awayButtons">
-          <button onClick={() => initialVAlue2(awayCount + 3)} className="awayButtons__touchdown">Away Touchdown</button>
-          <button onClick={() => initialVAlue2(awayCount + 7)} className="awayButtons__fieldGoal">Away Field Goal</button>
+          <button onClick={() => initialVAlue2(awayCount + 7)} className="awayButtons__touchdown">Away Touchdown</button>
+          <button onClick={() => initialVAlue2(awayCount + 3)} className="awayButtons__fieldGoal">Away Field Goal</button>
         </div>
       </section>
     </div>
