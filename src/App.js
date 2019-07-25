8d91f8b42901b8bf0abcf6be2c2a4dcafbf9ce25
Jashele T <jashele@Jashele.domain>
2019-07-24 18:18:10 -0700

    got initial values working in the DOM

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 //TODO: STEP 1 - Import the useState hook.
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import BottomRow from "./BottomRow";
 
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
+
+  const [homeCount, initialValue1] = useState(5);
+  const [awayCount, initialVAlue2] = useState(0);
 
   return (
     <div className="container">
@@ -15,12 +18,12 @@ function App() {
 
             {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
 
-            <div className="home__score">32</div>
+            <div className="home__score">{homeCount}</div>
           </div>
           <div className="timer">00:03</div>
           <div className="away">
             <h2 className="away__name">Tigers</h2>
-            <div className="away__score">32</div>
+            <div className="away__score">{awayCount}</div>
           </div>
         </div>
         <BottomRow />
