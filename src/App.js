f02e7d12c5a02907d36f53b975b0a72c0eea0872
Nicholas Interest <NicholasInterest1@users.noreply.github.com>
2019-08-12 18:39:18 -0400

    Changed some css and values

--- src/App.js
+++ src/App.js
@@ -6,21 +6,21 @@ import BottomRow from "./BottomRow";
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
 const [homeScore, setHomeScore] = useState(32);
-const [awayScore, setAwayScore] = useState(7);
+const [awayScore, setAwayScore] = useState(14);
   return (
     <div className="container">
       <section className="scoreboard">
         <div className="topRow">
           <div className="home">
-            <h2 className="home__name">Lions</h2>
+            <h2 className="home__name">NE</h2>
 
             {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
 
             <div className="home__score">{homeScore}</div>
           </div>
-          <div className="timer">00:03</div>
+          <div className="timer">02:00</div>
           <div className="away">
-            <h2 className="away__name">Tigers</h2>
+            <h2 className="away__name">MIA</h2>
             <div className="away__score">{awayScore}</div>
           </div>
         </div>
--- src/BottomRow.js
+++ src/BottomRow.js
@@ -10,11 +10,11 @@ const BottomRow = () => {
       </div>
       <div className="toGo">
         <h3 className="toGo__title">To Go</h3>
-        <div className="toGo__value">7</div>
+        <div className="toGo__value">10</div>
       </div>
       <div className="ballOn">
         <h3 className="ballOn__title">Ball on</h3>
-        <div className="ballOn__value">21</div>
+        <div className="ballOn__value">40</div>
       </div>
       <div className="quarter">
         <h3 className="quarter__title">Quarter</h3>
