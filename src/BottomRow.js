3b95433f9df43fb6c728a3a707547645979194c1
zachary-peasley <zpman95@hotmail.com>
2019-08-12 14:31:48 -0500

    MVP

--- src/BottomRow.js
+++ src/BottomRow.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
 
-const BottomRow = () => {
+const BottomRow = ({homeScore, setHomeScore, awayScore, setAwayScore}) => {
   return (
     <div className="bottomRow">
       <div className="down">
