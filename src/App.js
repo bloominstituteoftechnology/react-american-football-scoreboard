b65392e034c5b95c4f1a5b9fd06f52f67e377053
spettigrew <sara.pettigrew5@gmail.com>
2019-08-21 18:18:44 -0600

    app.js complete with 4 todo's

--- src/App.js
+++ src/App.js
@@ -5,8 +5,12 @@ import BottomRow from "./BottomRow";
 
 function App() {
   //TODO: STEP 2 - Establish your application's state with some useState hooks.  You'll need one for the home score and another for the away score.
+  
+  const [homeScore, updateHomeScore] = useState(19);
+  const [awayScore, updateAwayScore] = useState(27);
+  
+  // const[timer, updateTimer] = useState{15*60*1000} Timer for stretch
 
-  const [count, updateCount] = useState();
   return (
     <div className="container">
       <section className="scoreboard">
@@ -15,32 +19,32 @@ function App() {
             <h2 className="home__name">Lions</h2>
 
             {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
+    
 
-            <div className="home__score">32</div>
+            <div className="home__score">{homeScore}</div>
           </div>
-          <div className="timer">00:03</div>
+          <div className="timer">{0.3}</div>
           <div className="away">
             <h2 className="away__name">Tigers</h2>
-            <div className="away__score">32</div>
+            <div className="away__score">{awayScore}</div>
           </div>
         </div>
         <BottomRow />
+        
       </section>
+      
       <section className="buttons">
         <div className="homeButtons">
-          {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-
-          <button onClick={() => updateCount(count + 1)}>Increment</button>
-          <button onClick={() => updateCount(count - 1)}>Decrement</button>
-          <button onClick={() => updateCount(32)}>Reset</button>
-
-          <button className="homeButtons__touchdown">Home Touchdown</button>
-          <button className="homeButtons__fieldGoal">Home Field Goal</button>
+          {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */
+          }
+          <button onClick={() => updateHomeScore(homeScore + 7)} className="homeButtons__touchdown">Home Touchdown</button>
+          <button onClick={() => updateHomeScore(homeScore + 3)} className="homeButtons__fieldGoal">Home Field Goal</button>
         </div>
         <div className="awayButtons">
-          <button className="awayButtons__touchdown">Away Touchdown</button>
-          <button className="awayButtons__fieldGoal">Away Field Goal</button>
+          <button onClick={() => updateAwayScore(awayScore + 7)} className="awayButtons__touchdown">Away Touchdown</button>
+          <button onClick={() => updateAwayScore(awayScore + 3)} className="awayButtons__fieldGoal">Away Field Goal</button>
         </div>
+        
       </section>
     </div>
   );
