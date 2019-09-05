92e539c58109b229f1e93368d4ab5003c83bfa3d
byronholmes2018 <lshopeful2018@gmail.com>
2019-09-05 12:56:42 -0500

    For stretch, added handler function that increments given teams score by a given amount

--- src/App.js
+++ src/App.js
@@ -7,6 +7,14 @@ function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
   const [homeScore, incrementHomeScore] = useState(0);
   const [awayScore, incrementAwayScore] = useState(0);
+  const [team, setTeam] = useState("home");
+  const setScore = (team, amount) => {
+    if (team === "home") {
+      incrementHomeScore(amount);
+    } else {
+      incrementAwayScore(amount);
+    }
+  };
   return (
     <div className="container">
       <section className="scoreboard">
@@ -31,13 +39,13 @@ function App() {
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
           <button
             className="homeButtons__touchdown"
-            onClick={() => incrementHomeScore(homeScore + 6)}
+            onClick={() => setScore("home", 6)}
           >
             Home Touchdown
           </button>
           <button
             className="homeButtons__fieldGoal"
-            onClick={() => incrementHomeScore(homeScore + 3)}
+            onClick={() => setScore("home", 3)}
           >
             Home Field Goal
           </button>
@@ -45,13 +53,13 @@ function App() {
         <div className="awayButtons">
           <button
             className="awayButtons__touchdown"
-            onClick={() => incrementAwayScore(awayScore + 6)}
+            onClick={() => setScore("away", 6)}
           >
             Away Touchdown
           </button>
           <button
             className="awayButtons__fieldGoal"
-            onClick={() => incrementAwayScore(awayScore + 3)}
+            onClick={() => setScore("away", 3)}
           >
             Away Field Goal
           </button>
