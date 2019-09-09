6c35e29e3674ae3d5a33b20ff5874392dd6b4cb8
meera-andersen <meera.andersen@gmail.com>
2019-09-09 14:55:18 -0600

    added button that increase quarter

--- src/App.js
+++ src/App.js
@@ -28,6 +28,7 @@ function App() {
         </div>
         <BottomRow />
       </section>
+
       <section className="buttons">
         <div className="homeButtons">
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
--- src/BottomRow.js
+++ src/BottomRow.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, {useState} from "react";
 import "./App.css";
 
 const BottomRow = () => {
+  const [currentQuarter, setQuarter] = useState(1);
+  
   return (
     <div className="bottomRow">
       <div className="down">
@@ -18,10 +20,21 @@ const BottomRow = () => {
       </div>
       <div className="quarter">
         <h3 className="quarter__title">Quarter</h3>
-        <div className="quarter__value">4</div>
+        <div className="quarter__value">{currentQuarter}</div>
+        <button className="increaseQuarter" onClick = {() => setQuarter(currentQuarter + 1)}> Increase Quarter</button>
       </div>
     </div>
+
+
+// <section className="buttons">
+//   <div className="awayButtons">
+//     <button className="increaseQuarter" onClick = {() => setQuarter(currentQuarter + 1)}> Increase Quarter</button>
+//   </div>
+// </section>
   );
 };
-
 export default BottomRow;
+
+
+
+
