a9c9bb93f59ed8682bc6b55ff950e4f533a1f0a5
Luis Schekerka <luis@Luiss-MBP.fritz.box>
2019-09-09 15:10:40 +0200

    changed some styling and added buttons

--- src/BottomRow.js
+++ src/BottomRow.js
@@ -3,6 +3,26 @@ import "./App.css";
 
 const BottomRow = () => {
 
+
+
+  const [down, setDown] = useState(1);
+
+  const nextDown = () => {
+    setDown(down + 1);
+  }
+
+  const [toGo, setToGo] = useState(50);
+
+  const decreaseToGo = () => {
+    setToGo(toGo - 1);
+  }
+
+  const [ballOn, setBallOn] = useState(50);
+
+  const decreaseBallOn = () => {
+    setBallOn(ballOn - 1);
+  }
+
   const [quarter, setQuarter] = useState(1);
 
   const nextQuarter = () => {
@@ -13,15 +33,18 @@ const BottomRow = () => {
     <div className="bottomRow">
       <div className="down">
         <h3 className="down__title">Down</h3>
-        <div className="down__value">3</div>
+        <div className="down__value">{down}</div>
+        <button onClick={nextDown} className="button">Next Down</button>
       </div>
       <div className="toGo">
         <h3 className="toGo__title">To Go</h3>
-        <div className="toGo__value">7</div>
+        <div className="toGo__value">{toGo}</div>
+        <button onClick={decreaseToGo} className="button">Decrease 1 Yard</button>
       </div>
       <div className="ballOn">
         <h3 className="ballOn__title">Ball on</h3>
-        <div className="ballOn__value">21</div>
+        <div className="ballOn__value">{ballOn}</div>
+        <button onClick={decreaseBallOn} className="button">Decrease 1 Yard</button>
       </div>
       <div className="quarter">
         <h3 className="quarter__title">Quarter</h3>
