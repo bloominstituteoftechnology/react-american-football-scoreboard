4b134f63c9c17b6613199ef6ce948b1a39691d73
abarne <celius377@gmail.com>
2019-09-09 15:52:09 -0500

    down, to go, and ball on, changes during game.

--- src/BottomRow.js
+++ src/BottomRow.js
@@ -1,27 +1,51 @@
-import React from "react";
-import "./App.css";
+import React, { useState } from 'react';
+import './App.css';
 
-const BottomRow = () => {
-  return (
-    <div className="bottomRow">
-      <div className="down">
-        <h3 className="down__title">Down</h3>
-        <div className="down__value">3</div>
-      </div>
-      <div className="toGo">
-        <h3 className="toGo__title">To Go</h3>
-        <div className="toGo__value">7</div>
-      </div>
-      <div className="ballOn">
-        <h3 className="ballOn__title">Ball on</h3>
-        <div className="ballOn__value">21</div>
-      </div>
-      <div className="quarter">
-        <h3 className="quarter__title">Quarter</h3>
-        <div className="quarter__value">4</div>
-      </div>
-    </div>
-  );
-};
+function App() {
+	const [ down, setDown ] = useState(1);
+	const [ toGo, setToGo ] = useState(10);
+	const [ ballOn, setBallOn ] = useState(22);
 
-export default BottomRow;
+	var min = 20,
+		max = 50;
+	var rand = Math.floor(Math.random() * (max - min + 1) + min);
+	var min1 = 1,
+		max1 = 50;
+	var rand1 = Math.floor(Math.random() * (max1 - min1 + 1) + min1);
+	setTimeout(() => {
+		setBallOn(rand1);
+		if (down === 4) {
+			setDown(1);
+		} else {
+			setDown(down + 1);
+		}
+		if (down === 1) {
+			setToGo(10);
+		} else {
+			setToGo(rand);
+		}
+	}, rand * 1000);
+
+	return (
+		<div className="bottomRow">
+			<div className="down">
+				<h3 className="down__title">Down</h3>
+				<div className="down__value">{down}</div>
+			</div>
+			<div className="toGo">
+				<h3 className="toGo__title">To Go</h3>
+				<div className="toGo__value">{toGo}</div>
+			</div>
+			<div className="ballOn">
+				<h3 className="ballOn__title">Ball on</h3>
+				<div className="ballOn__value">{ballOn}</div>
+			</div>
+			<div className="quarter">
+				<h3 className="quarter__title">Quarter</h3>
+				<div className="quarter__value">4</div>
+			</div>
+		</div>
+	);
+}
+
+export default App;
