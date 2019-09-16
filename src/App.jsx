9dd2d0df064696b8a34f47c7ea9bed332f43d2bc
Westley Feller <feller.westley@gmail.com>
2019-09-16 10:50:41 -0700

    Using state variables

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import './App.css';
 import BottomRow from './BottomRow';
 
 function App() {
-  const [homeScore, awayScore] = useState(0);
+  const [homeScore] = useState(0);
+  const [awayScore] = useState(0);
 
   return (
     <div className="container">
@@ -12,15 +13,12 @@ function App() {
           <div className="home">
             <h2 className="home__name">Lions</h2>
 
-            {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic
-            values from our state. */}
-
-            <div className="home__score">32</div>
+            <div className="home__score">{homeScore}</div>
           </div>
           <div className="timer">00:03</div>
           <div className="away">
             <h2 className="away__name">Tigers</h2>
-            <div className="away__score">32</div>
+            <div className="away__score">{awayScore}</div>
           </div>
         </div>
         <BottomRow />
