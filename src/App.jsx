1d8fe70d3c6185000abe8212fdf9cbfdc8c67d74
Westley Feller <feller.westley@gmail.com>
2019-09-16 13:05:03 -0700

    Created components directory

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import './App.css';
-import TopRow from './TopRow/TopRow';
-import BottomRow from './BottomRow/BottomRow';
+import TopRow from './Components/TopRow/TopRow';
+import BottomRow from './Components/BottomRow/BottomRow';
 
 function App() {
   const [homeScore, setHomeScore] = useState(0);
