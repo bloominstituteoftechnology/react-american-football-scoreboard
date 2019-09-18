c20abba0727e1d628b62ca74850d8953ee5a7202
rich92105 <52473445+rich92105@users.noreply.github.com>
2019-09-17 20:41:15 -0700

    imported useState

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@
 import React from "react";
 import "./App.css";
 import BottomRow from "./BottomRow";
+import { useState } from 'react';
+
 
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
 
   return (
+    
     <div className="container">
       <section className="scoreboard">
         <div className="topRow">
