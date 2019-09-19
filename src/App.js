340fb3343f128e765dca84ec0f4b4672b41938d0
Brett Karr <b.karr@polyclip.us>
2019-09-18 22:54:11 -0500

    commit with stretch goals reached

--- src/App.js
+++ src/App.js
@@ -7,6 +7,33 @@ function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
   const [pointsHome, setPointsHome] = useState(0);
   const [pointsAway, setPointsAway] = useState(0);
+  //stretch constants
+  const [downVal, setDown] = useState(3);
+  const [yardsTilFirst, setYards] = useState(7);
+  const [ballOnYard, setBallOn] = useState(21);
+  const [quarter, setQuarter] = useState(1);
+  //Functions for stretch goals
+  const quarterInc = () => {
+    if (quarter < 4) {
+      setQuarter(quarter + 1);
+    } else {
+      setQuarter(4);
+    }
+  };
+  const downInc = () => {
+    if (downVal < 4) {
+      setDown(downVal + 1);
+    } else {
+      setDown(4);
+    }
+  };
+  const ballOnInc = () => {
+    if (ballOnYard < 50) {
+      setBallOn(ballOnYard + 1);
+    } else {
+      setBallOn(50);
+    }
+  };
 
   return (
     <div className="container">
@@ -27,36 +54,78 @@ function App() {
             <div className="away__score">{pointsAway}</div>
           </div>
         </div>
-        <BottomRow />
+        <BottomRow
+          down={downVal}
+          yardsLeft={yardsTilFirst}
+          ballOn={ballOnYard}
+          quarter={quarter}
+        />
       </section>
       <section className="buttons">
-        <div className="homeButtons">
-          {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button
-            className="homeButtons__touchdown"
-            onClick={() => setPointsHome(pointsHome + 7)}
-          >
-            Home Touchdown
-          </button>
-          <button
-            className="homeButtons__fieldGoal"
-            onClick={() => setPointsHome(pointsHome + 3)}
-          >
-            Home Field Goal
-          </button>
+        <div className="scoreBoardPoints">
+          <div className="homeButtons">
+            {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
+            <button
+              className="homeButtons__touchdown"
+              onClick={() => setPointsHome(pointsHome + 7)}
+            >
+              Home Touchdown
+            </button>
+            <button
+              className="homeButtons__fieldGoal"
+              onClick={() => setPointsHome(pointsHome + 3)}
+            >
+              Home Field Goal
+            </button>
+          </div>
+          <div className="awayButtons">
+            <button
+              className="awayButtons__touchdown"
+              onClick={() => setPointsAway(pointsAway + 7)}
+            >
+              Away Touchdown
+            </button>
+            <button
+              className="awayButtons__fieldGoal"
+              onClick={() => setPointsAway(pointsAway + 3)}
+            >
+              Away Field Goal
+            </button>
+          </div>
         </div>
-        <div className="awayButtons">
-          <button
-            className="awayButtons__touchdown"
-            onClick={() => setPointsAway(pointsAway + 7)}
-          >
-            Away Touchdown
+        <div className="scoreBoardControls">
+          <button className="down" onClick={downInc}>
+            Next Down
           </button>
           <button
-            className="awayButtons__fieldGoal"
-            onClick={() => setPointsAway(pointsAway + 3)}
+            className="yardsLeft"
+            onClick={() => setYards(yardsTilFirst + 1)}
           >
-            Away Field Goal
+            To Go +1
+          </button>
+          <button
+            className="yardsLeft"
+            onClick={() => setYards(yardsTilFirst - 1)}
+          >
+            To Go -1
+          </button>
+          <button className="ballOn" onClick={ballOnInc}>
+            Ball On
+          </button>
+          <button className="quarter" onClick={quarterInc}>
+            Quarter
+          </button>
+          <button
+            className="reset"
+            onClick={() => {
+              setPointsHome(0);
+              setPointsAway(0);
+              setDown(1);
+              setYards(10);
+              setQuarter(1);
+            }}
+          >
+            Reset
           </button>
         </div>
       </section>
--- src/BottomRow.js
+++ src/BottomRow.js
@@ -1,26 +1,29 @@
 import React from "react";
 import "./App.css";
 
-const BottomRow = () => {
+const BottomRow = props => {
+  console.log(props);
+
   return (
     <div className="bottomRow">
       <div className="down">
         <h3 className="down__title">Down</h3>
-        <div className="down__value">3</div>
+        <div className="down__value">{props.down}</div>
       </div>
       <div className="toGo">
         <h3 className="toGo__title">To Go</h3>
-        <div className="toGo__value">7</div>
+        <div className="toGo__value">{props.yardsLeft}</div>
       </div>
       <div className="ballOn">
         <h3 className="ballOn__title">Ball on</h3>
-        <div className="ballOn__value">21</div>
+        <div className="ballOn__value">{props.ballOn}</div>
       </div>
       <div className="quarter">
         <h3 className="quarter__title">Quarter</h3>
-        <div className="quarter__value">4</div>
+        <div className="quarter__value">{props.quarter}</div>
       </div>
     </div>
+    // console.log(props);
   );
 };
 
