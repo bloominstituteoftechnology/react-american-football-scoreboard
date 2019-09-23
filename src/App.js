274c24749a07354ab67290a33d7dfd92f10c7574
garybot <argot.pryer@gmail.com>
2019-09-22 18:16:01 -0700

    added buttons to change down and quarter.

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import BottomRow from "./BottomRow";
 function App() {
   const [homeScore, setHome] = useState(0);
   const [awayScore, setAway] = useState(0);
+  const [quarter, setQuarter] = useState(0);
+  const [down, setDown] = useState(0)
 
   function scoreHandler(team, amount) {
     if (team === "Lions") {
@@ -13,8 +15,20 @@ function App() {
       setAway(awayScore + amount);
     }
   }
+  function downHandler(change) {
+    if (change === "next" && down < 4) {
+      setDown(down + 1);
+    } else if (change === "first") {
+      setDown(1);
+    }
+  }
+  function quarterHandler() {
+    if (quarter < 4) {
+      setQuarter(quarter + 1);
+    }
+  }
 
-  return (
+    return (
     <div className="container">
       <section className="scoreboard">
         <div className="topRow">
@@ -28,7 +42,7 @@ function App() {
             <div className="away__score">{ awayScore }</div>
           </div>
         </div>
-        <BottomRow />
+        <BottomRow quarter={quarter} down={down} />
       </section>
       <section className="buttons">
         <div className="homeButtons">
@@ -39,9 +53,12 @@ function App() {
           <button className="awayButtons__touchdown" onClick={()=>scoreHandler("Tigers", 7)}>Away Touchdown</button>
           <button className="awayButtons__fieldGoal" onClick={()=>scoreHandler("Tigers", 3)}>Away Field Goal</button>
         </div>
+        <div className="downButtons">
+          <button className="downButtons__down-next" onClick={()=>downHandler("next")}>Next Down</button>
+          <button className="downButtons__down-first" onClick={()=>downHandler("first")}>First Down</button>
+        </div>
         <div className="timerButtons">
-          <button className="timerButtons__quarter">Next Quarter</button>
-          <button className="timerButtons__down">Next Down</button>
+          <button className="timerButtons__quarter" onClick={()=>quarterHandler()}>Next Quarter</button>
         </div>
       </section>
     </div>
--- src/BottomRow.js
+++ src/BottomRow.js
@@ -1,12 +1,12 @@
 import React from "react";
 import "./App.css";
 
-const BottomRow = () => {
+const BottomRow = (props) => {
   return (
     <div className="bottomRow">
       <div className="down">
         <h3 className="down__title">Down</h3>
-        <div className="down__value">3</div>
+        <div className="down__value">{props.down}</div>
       </div>
       <div className="toGo">
         <h3 className="toGo__title">To Go</h3>
@@ -18,7 +18,7 @@ const BottomRow = () => {
       </div>
       <div className="quarter">
         <h3 className="quarter__title">Quarter</h3>
-        <div className="quarter__value">3</div>
+        <div className="quarter__value">{props.quarter}</div>
       </div>
     </div>
   );
