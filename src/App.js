a06c0876edc6d9d3d3d8cd9f207d49498d2f02c4
Norma Bunton <te_nj25@hotmail.com>
2019-10-08 14:34:06 -0400

    fotball scoreboard

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import BottomRow from "./BottomRow";
 
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
-const [homeCount, setHomeCount] = useState(0)
-const [awayCount, setAwayCount] = useState(0)
+const [homeCount, setHomeCount] = useState(0);
+const [awayCount, setAwayCount] = useState(0);
 
   return (
     <div className="container">
@@ -17,12 +17,12 @@ const [awayCount, setAwayCount] = useState(0)
 
             {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
 
-            <div className="home__score">32</div>
+            <div className="home__score">{homeCount}</div>
           </div>
           <div className="timer">00:03</div>
           <div className="away">
             <h2 className="away__name">Tigers</h2>
-            <div className="away__score">32</div>
+            <div className="away__score">{awayCount}</div>
           </div>
         </div>
         
