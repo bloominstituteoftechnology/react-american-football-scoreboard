6597eff06a99497c624c0a81580767dea2c4551e
ChiragThesia <harithesia@gmail.com>
2019-10-16 18:27:27 -0500

    Commit from a new PC

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import BottomRow from "./BottomRow";
 
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
-
+//Test commit/push
   return (
     <div className="container">
       <section className="scoreboard">
@@ -14,7 +14,7 @@ function App() {
             <h2 className="home__name">Lions</h2>
 
             {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
-
+            
             <div className="home__score">32</div>
           </div>
           <div className="timer">00:03</div>
