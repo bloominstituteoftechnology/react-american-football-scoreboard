b93e441748a13fbd1e34eb994cc36b2b7f40af62
Michael Lowe <mrlowememphis@yahoo.com>
2019-10-16 22:09:29 -0500

    Added Quarter counter increments by 1

--- src/BottomRow.js
+++ src/BottomRow.js
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useState} from "react";
 import "./App.css";
 
 const BottomRow = () => {
+  const [quarter, setQuarter] = useState(1);
+  const quarterStop = (e) =>{
+    if (quarter === 4){
+      return;
+    }
+    setQuarter(quarter + 1)
+  }
   return (
     <div className="bottomRow">
       <div className="down">
@@ -18,7 +25,9 @@ const BottomRow = () => {
       </div>
       <div className="quarter">
         <h3 className="quarter__title">Quarter</h3>
-        <div className="quarter__value">4</div>
+        {/* <div className="quarter__value">4</div> */}
+        <div className="quarter__value">{quarter}</div>
+        <button className="awayButtons__fieldGoal" onClick = {quarterStop}>Quarter</button>
       </div>
     </div>
   );
