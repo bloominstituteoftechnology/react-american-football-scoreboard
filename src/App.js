991b7211352495cda55fd627308f045795f9193a
Franzferdinan51 <franzferdinan51@gmail.com>
2019-10-16 21:05:27 -0400

    timer button

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 //TODO: STEP 1 - Import the useState hook.
 import React, {useState, useEffect} from "react";
 import "./App.css";
+
 import BottomRow from "./BottomRow";
 
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
   const [home, setHome] = useState(0);
   const [away, setAway] = useState(0);
+  const [seconds, setSeconds] = useState(0);
+  const [minuets, setMinuets] = useState(0);
+  const [isActive, setIsActive] = useState(false);
   return (
     <div className="container">
       <section className="scoreboard">
         <div className="topRow">
           <div className="home">
             <h2 className="home__name">Bengals</h2>
+            
 
             {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
 
             <div className="home__score">{home}</div>
           </div>
-          <div className="timer">00:03</div>
+          <div className="timer">{minuets}:{seconds}</div>
           <div className="away">
             <h2 className="away__name">Browns</h2>
             <div className="away__score">{away}</div>
@@ -37,11 +42,11 @@ function App() {
           <button className="awayButtons__fieldGoal"onClick = {() => setAway(away + 7) }>Away Field Goal</button>
         </div>
         <div className = "timer">
-          <button className="awayButtons__fieldGoal"onClick = {() => setAway(away + 7) }>Away Field Goal</button>
+          <button className="timer"onClick = {() => setSeconds(seconds +1)}>Timer</button>
+          
           </div>
       </section>
     </div>
   );
 }
-
 export default App;
