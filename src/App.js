5d2c513ac7ebfc1c91b000202ab6ab7fb1e1efe8
Samuel Kraus <S_read@aol.com>
2019-10-16 19:40:58 -0500

    created the uste state hooks and functions

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
 //TODO: STEP 1 - Import the useState hook.
-import React from "react";
+import React, { useState } from "react";
+// import React from "react";
 import "./App.css";
 import BottomRow from "./BottomRow";
 
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
+
+// made the hooks for home and away with the set properties
+const [homeScore, setHomeScore] = useState(0);
+const [awayScore, setAwayScore] = useState(0);
+
+
+// creating functions using the hook's set properties to increase 
+const homeTouchDown = ()=>{
+  setHomeScore(homeScore + 7);
+};
+
+const awayTouchDown = () =>{
+  setAwayScore(awayScore + 7);
+};
+
+
+const homeFieldGoal = () =>{
+  setHomeScore(homeScore + 3);
+};
+
+const awayFieldGoal = () =>{
+  setAwayScore(awayScore + 3);
+};
+
+
 
   return (
     <div className="container">
