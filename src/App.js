48286454cc5d8cc06f417deab236373b97b0040f
Franzferdinan51 <franzferdinan51@gmail.com>
2019-10-16 20:01:09 -0400

    Starting Timer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 //TODO: STEP 1 - Import the useState hook.
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import "./App.css";
 import BottomRow from "./BottomRow";
 
