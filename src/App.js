79ddd119b043987762bfc7d3d33df25c2c96920c
jarrodskahill <jarrodskahill@gmail.com>
2019-11-04 13:42:53 -0800

    added on clicks to buttons, finished MVP

--- src/App.js
+++ src/App.js
@@ -46,12 +46,12 @@ function App() {
       <section className="buttons">
         <div className="homeButtons">
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button className="homeButtons__touchdown">Home Touchdown</button>
-          <button className="homeButtons__fieldGoal">Home Field Goal</button>
+          <button className="homeButtons__touchdown" onClick = {homeTD}>Home Touchdown</button>
+          <button className="homeButtons__fieldGoal" onClick = {homeFG}>Home Field Goal</button>
         </div>
         <div className="awayButtons">
-          <button className="awayButtons__touchdown">Away Touchdown</button>
-          <button className="awayButtons__fieldGoal">Away Field Goal</button>
+          <button className="awayButtons__touchdown" onClick = {awayTD}>Away Touchdown</button>
+          <button className="awayButtons__fieldGoal" onClick = {awayFG}>Away Field Goal</button>
         </div>
       </section>
     </div>
