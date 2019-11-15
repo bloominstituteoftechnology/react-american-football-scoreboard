8dc8d12bc1446349660bfdfa570f08807d534023
Ben Urena <urenadev@gmail.com>
2019-11-14 19:30:20 -0600

    Completed step 3 for dynamic values

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ function App() {
           <div className="timer">00:03</div>
           <div className="away">
             <h2 className="away__name">Tigers</h2>
-            <div className="away__score">32</div>
+            <div className="away__score">{awayScore + 32}</div>
           </div>
         </div>
         <BottomRow />
