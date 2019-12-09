57570bbc51268c4ba8163fb7ce2b18b6d1e01365
c0der.br1t <c0der.br1t@gmail.com>
2019-12-09 15:18:09 -0600

    small scoreboard changes to timer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 //TODO: STEP 1 - Import the useState hook.
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import BottomRow from "./BottomRow";
+
+
 
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
@@ -20,7 +22,7 @@ function App() {
 
             <div className="home__score">{ home }</div>
           </div>
-          <div className="timer">00:03</div>
+          <div className="timer">15:00</div>
           <div className="away">
             <h2 className="away__name">Tigers</h2>
             <div className="away__score">{ away }</div>
