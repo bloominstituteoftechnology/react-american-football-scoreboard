5a27e5dfcc78ebed60b099ae1f5e3d49579c24b5
Kaela M Gallo <kaelamgallo@Kaelas-MBP.home>
2019-12-09 21:56:21 -0500

    finished-homework

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 //TODO: STEP 1 - Import the useState hook.
-import React, {useState} from "react";
+import React, { useState } from "react";
 import "./App.css";
 import BottomRow from "./BottomRow";
 
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
- const [homeScore, setCount] = useState(0);
- const [awayScore, serCount] = useState(0)
+  const [homeScore, setCount] = useState(0);
+  const [awayScore, serCount] = useState(0);
   return (
     <div className="container">
       <section className="scoreboard">
@@ -29,12 +29,32 @@ function App() {
       <section className="buttons">
         <div className="homeButtons">
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button className="homeButtons__touchdown" onClick={ () => setCount(homeScore + 7)}>Home Touchdown</button>
-          <button className="homeButtons__fieldGoal" onClick={ () => setCount(homeScore + 3)}>Home Field Goal</button>
+          <button
+            className="homeButtons__touchdown"
+            onClick={() => setCount(homeScore + 7)}
+          >
+            Home Touchdown
+          </button>
+          <button
+            className="homeButtons__fieldGoal"
+            onClick={() => setCount(homeScore + 2)}
+          >
+            Home Field Goal
+          </button>
         </div>
         <div className="awayButtons">
-          <button className="awayButtons__touchdown" onClick={ () => serCount(awayScore + 7) }>Away Touchdown</button>
-          <button className="awayButtons__fieldGoal" onClick={ () => serCount(awayScore + 3)}>Away Field Goal</button>
+          <button
+            className="awayButtons__touchdown"
+            onClick={() => serCount(awayScore + 7)}
+          >
+            Away Touchdown
+          </button>
+          <button
+            className="awayButtons__fieldGoal"
+            onClick={() => serCount(awayScore + 2)}
+          >
+            Away Field Goal
+          </button>
         </div>
       </section>
     </div>
@@ -43,7 +63,8 @@ function App() {
 
 export default App;
 
-
 // class HelloMessage extends React.Component {render() {return ( <h1>Hello my name is {this.props.name}</h1>);}}
 
 // ReactDOM.render(<HelloMessage name="Yannick" />,document.getElementById('hello-example'));
+
+// const simpsonData = {name: "Orville Simpson",spouse: "Yuma Hickman", children: [{name: "Abraham Simpson",spouse: "Mona",children: [{name: "Homer Simpson",spouse: "Marge Bouvier",children: [{ name: "Bart Simpson"},{ name: "Lisa Simpson"},{name: "Maggie Simpson"}]}]}]};
