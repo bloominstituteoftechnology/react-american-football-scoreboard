1b7c886935e0a7c6d5992217f48200de30ddea11
Amanda Hogan <ahogan@teamworks.com>
2020-01-17 16:35:34 -0500

    Touchdown and field goal button event listeners

--- src/App.js
+++ src/App.js
@@ -9,6 +9,22 @@ function App() {
   const [homeScore, setHomeScore] = useState(0);
   const [awayScore, setAwayScore] = useState(0);
 
+  const homeTouchdown = () => {
+    setHomeScore(homeScore + 7);
+  };
+
+  const homeFG = () => {
+    setHomeScore(homeScore + 3);
+  };
+
+  const awayTouchdown = () => {
+    setAwayScore(awayScore + 7);
+  };
+
+  const awayFG = () => {
+    setAwayScore(awayScore + 3);
+  };
+
   return (
     <div className="container">
       <section className="scoreboard">
@@ -31,12 +47,12 @@ function App() {
       <section className="buttons">
         <div className="homeButtons">
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button className="homeButtons__touchdown">Home Touchdown</button>
-          <button className="homeButtons__fieldGoal">Home Field Goal</button>
+          <button className="homeButtons__touchdown" onClick={homeTouchdown}>Home Touchdown</button>
+          <button className="homeButtons__fieldGoal" onClick={homeFG}>Home Field Goal</button>
         </div>
         <div className="awayButtons">
-          <button className="awayButtons__touchdown">Away Touchdown</button>
-          <button className="awayButtons__fieldGoal">Away Field Goal</button>
+          <button className="awayButtons__touchdown" onClick={awayTouchdown}>Away Touchdown</button>
+          <button className="awayButtons__fieldGoal" onClick={awayFG}>Away Field Goal</button>
         </div>
       </section>
     </div>
