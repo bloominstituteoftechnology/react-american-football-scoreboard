7866e24b3c264448206a5b196af96e4ed8327701
shawn <shawnpatel196@gmail.com>
2020-01-20 15:54:25 -0600

    finished useState hooks

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 //TODO: STEP 1 - Import the useState hook.
-import React from "react";
+import React, {useState} from "react";
 import "./App.css";
 import BottomRow from "./BottomRow";
 
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
+const [homeScore, setHomeScore] = useState(0);
+const [awayScore, setAwayScore] = useState(0);
+
+const homeTouchDown = e =>{
+  setHomeScore(homeScore+7);
+}
+const awayTouchDown = e =>{
+  setHomeScore(awayScore+7);
+}
+const homeFieldGoal = e =>{
+  setHomeScore(homeScore+3);
+}
+const awayFieldGoal = e =>{
+  setHomeScore(awayScore+3);
+}
 
   return (
     <div className="container">
@@ -15,12 +30,12 @@ function App() {
 
             {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
 
-            <div className="home__score">32</div>
+            <div className="home__score">{homeScore}</div>
           </div>
           <div className="timer">00:03</div>
           <div className="away">
             <h2 className="away__name">Tigers</h2>
-            <div className="away__score">32</div>
+            <div className="away__score">{awayScore}</div>
           </div>
         </div>
         <BottomRow />
