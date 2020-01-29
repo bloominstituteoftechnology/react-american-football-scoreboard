678cbe46e08c5197a0229ddbdabd43349a0d9c9c
Kenneth-Brandon <onegiantsfan25@yahoo.com>
2020-01-28 21:29:22 -0700

    kenneth-brandon-webpt13

--- src/App.js
+++ src/App.js
@@ -30,12 +30,32 @@ function App() {
       <section className="buttons">
         <div className="homeButtons">
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button className="homeButtons__touchdown" onClick={() => setCount(count + 7)}>Home Touchdown</button>
-          <button className="homeButtons__fieldGoal" onClick={() => setCount(count + 3)}>Home Field Goal</button>
+          <button
+            className="homeButtons__touchdown"
+            onClick={() => setCount(count + 7)}
+          >
+            Home Touchdown
+          </button>
+          <button
+            className="homeButtons__fieldGoal"
+            onClick={() => setCount(count + 3)}
+          >
+            Home Field Goal
+          </button>
         </div>
         <div className="awayButtons">
-          <button className="awayButtons__touchdown" onClick={() => setCountAway(countAway + 7)}>Away Touchdown</button>
-          <button className="awayButtons__fieldGoal" onClick={() => setCountAway(countAway + 3)}>Away Field Goal</button>
+          <button
+            className="awayButtons__touchdown"
+            onClick={() => setCountAway(countAway + 7)}
+          >
+            Away Touchdown
+          </button>
+          <button
+            className="awayButtons__fieldGoal"
+            onClick={() => setCountAway(countAway + 3)}
+          >
+            Away Field Goal
+          </button>
         </div>
       </section>
     </div>
