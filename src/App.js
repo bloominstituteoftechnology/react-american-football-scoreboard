95d59eea4be78deafd83e8c42a8ca76e94a530a3
Anthony <chitowncode@gmail.com>
2020-01-29 20:28:59 -0600

    I have finished at least MVP, will try to work on some of the stretch goals

--- src/App.js
+++ src/App.js
@@ -1,43 +1,53 @@
 //TODO: STEP 1 - Import the useState hook.
-import React from "react";
-import "./App.css";
-import BottomRow from "./BottomRow";
+import React, { useState } from 'react';
+import './App.css';
+import BottomRow from './BottomRow';
 
 function App() {
-  //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
+	//TODO: STEP 2 - Establish your application's state with some useState hooks.  You'll need one for the home score and another for the away score.
+	const [ home, setHome ] = useState(21);
+	const [ away, setAway ] = useState(14);
 
-  return (
-    <div className="container">
-      <section className="scoreboard">
-        <div className="topRow">
-          <div className="home">
-            <h2 className="home__name">Lions</h2>
+	return (
+		<div className='container'>
+			<section className='scoreboard'>
+				<div className='topRow'>
+					<div className='home'>
+						<h2 className='home__name'>Bears</h2>
 
-            {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
+						{/* TODO STEP 3 - We need to change the hard-coded values in these divs to accept dynamic values from our state. */}
 
-            <div className="home__score">32</div>
-          </div>
-          <div className="timer">00:03</div>
-          <div className="away">
-            <h2 className="away__name">Tigers</h2>
-            <div className="away__score">32</div>
-          </div>
-        </div>
-        <BottomRow />
-      </section>
-      <section className="buttons">
-        <div className="homeButtons">
-          {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button className="homeButtons__touchdown">Home Touchdown</button>
-          <button className="homeButtons__fieldGoal">Home Field Goal</button>
-        </div>
-        <div className="awayButtons">
-          <button className="awayButtons__touchdown">Away Touchdown</button>
-          <button className="awayButtons__fieldGoal">Away Field Goal</button>
-        </div>
-      </section>
-    </div>
-  );
+						<div className='home__score'>{home}</div>
+					</div>
+					<div className='timer'>03:01</div>
+					<div className='away'>
+						<h2 className='away__name'>Packers</h2>
+						<div className='away__score'>{away}</div>
+					</div>
+				</div>
+				<BottomRow />
+			</section>
+			<section className='buttons'>
+				<div className='homeButtons'>
+					{/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
+					<button onClick={() => setHome(home + 7)} className='homeButtons__touchdown'>
+						Humpty Dance
+					</button>
+					<button onClick={() => setHome(home + 3)} className='homeButtons__fieldGoal'>
+						Hammer Time
+					</button>
+				</div>
+				<div className='awayButtons'>
+					<button onClick={() => setAway(away + 7)} className='awayButtons__touchdown'>
+						Same Old Song
+					</button>
+					<button onClick={() => setAway(away + 3)} className='awayButtons__fieldGoal'>
+						Wait, What?
+					</button>
+				</div>
+			</section>
+		</div>
+	);
 }
 
 export default App;
