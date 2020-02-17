cda4d403e08e77832ce6f4949b11508842cf8fbc
JW Nicholson <john.nicholson812@gmail.com>
2020-02-17 15:06:39 -0500

    project setup, step 1 done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 //TODO: STEP 1 - Import the useState hook.
-import React from "react";
+import React, {useState} from "react";
 import "./App.css";
 import BottomRow from "./BottomRow";
 
 function App() {
-  //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
+  //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score
+  cons [score, setScore] = useState();
 
   return (
     <div className="container">
