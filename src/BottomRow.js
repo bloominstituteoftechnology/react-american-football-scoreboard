01e5e2e2404d53063ab5c4128955b0758eae2e5e
Ryan Erickson <rerickson21@yahoo.com>
2020-02-27 21:32:51 -0500

    MVP Done

--- src/BottomRow.js
+++ src/BottomRow.js
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 const BottomRow = () => {
+  const [currentQuarter, setCurrentQuarter] = useState(1);
+
+  const addNewQuarter = event => {
+    setCurrentQuarter(currentQuarter + 1);
+  }  
+
   return (
     <div className="bottomRow">
       <div className="down">
@@ -18,7 +24,7 @@ const BottomRow = () => {
       </div>
       <div className="quarter">
         <h3 className="quarter__title">Quarter</h3>
-        <div className="quarter__value">4</div>
+        <button onClick={addNewQuarter} className="quarter__value">{currentQuarter}</button>
       </div>
     </div>
   );
