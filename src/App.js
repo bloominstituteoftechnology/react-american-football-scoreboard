b99480bc919db09a55eb26faace21d55f34c7492
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-04-13 14:50:24 +0200

    take out service worker, clean up unused files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 //TODO: STEP 1 - Import the useState hook.
 import React from "react";
-import "./App.css";
 import BottomRow from "./BottomRow";
+import "./App.css";
 
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
@@ -14,7 +14,6 @@ function App() {
             <h2 className="home__name">Lions</h2>
 
             {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
-
             <div className="home__score">32</div>
           </div>
           <div className="timer">00:03</div>
@@ -27,6 +26,7 @@ function App() {
       </section>
       <section className="buttons">
         <div className="homeButtons">
+
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
           <button className="homeButtons__touchdown">Home Touchdown</button>
           <button className="homeButtons__fieldGoal">Home Field Goal</button>
