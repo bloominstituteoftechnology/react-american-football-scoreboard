24c8475831e42f51856bc3d6cd41e9812c17253e
Spencer_Vail <vail.spencer@gmail.com>
2020-04-13 14:30:10 -0700

    MVP Done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 //TODO: STEP 1 - Import the useState hook.
-import React from "react";
+import React, {useState} from "react";
 import BottomRow from "./BottomRow";
 import "./App.css";
 
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
-
+  const baseState= 0
+  const [teamHawks, setHawks] = useState(baseState)
+  const [teamRams, setRams] = useState(baseState)
+ 
   return (
     <div className="container">
       <section className="scoreboard">
         <div className="topRow">
           <div className="home">
-            <h2 className="home__name">Lions</h2>
+            <h2 className="home__name">Hawks</h2>
 
             {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
-            <div className="home__score">32</div>
+            <div className="home__score">{teamHawks}</div>
           </div>
           <div className="timer">00:03</div>
           <div className="away">
-            <h2 className="away__name">Tigers</h2>
-            <div className="away__score">32</div>
+            <h2 className="away__name">Rams</h2>
+            <div className="away__score">{teamRams}</div>
           </div>
         </div>
         <BottomRow />
@@ -28,12 +31,12 @@ function App() {
         <div className="homeButtons">
 
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button className="homeButtons__touchdown">Home Touchdown</button>
-          <button className="homeButtons__fieldGoal">Home Field Goal</button>
+          <button className="homeButtons__touchdown" onClick ={() => setHawks(teamHawks + 7)}>Home Touchdown</button>
+          <button className="homeButtons__fieldGoal" onClick ={() => setHawks(teamHawks + 3)}>Home Field Goal</button>
         </div>
         <div className="awayButtons">
-          <button className="awayButtons__touchdown">Away Touchdown</button>
-          <button className="awayButtons__fieldGoal">Away Field Goal</button>
+          <button className="awayButtons__touchdown" onClick ={() => setRams(teamRams + 7)}>Away Touchdown</button>
+          <button className="awayButtons__fieldGoal" onClick ={() => setRams(teamRams + 3)}>Away Field Goal</button>
         </div>
       </section>
     </div>
--- src/index.js
+++ src/index.js
@@ -4,3 +4,7 @@ import './index.css';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+
+
+
