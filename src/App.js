0d612df4f3fb0c2cf855d78a46b1ce5dc8e24adb
tbbcoach <58452640+tbbcoach@users.noreply.github.com>
2020-04-22 19:07:02 -0400

    added functionality to home team buttons.

--- src/App.js
+++ src/App.js
@@ -31,8 +31,16 @@ const App = () => {
         <div className="homeButtons">
 
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button className="homeButtons__touchdown">Home Touchdown</button>
-          <button className="homeButtons__fieldGoal">Home Field Goal</button>
+          <button className="homeButtons__touchdown"
+            onClick={() => {
+              sethomeScore(homeScore + 7);
+            }}
+          >Home Touchdown</button>
+          <button className="homeButtons__fieldGoal"
+            onClick={() => {
+              sethomeScore(homeScore + 3);
+            }}
+           >Home Field Goal</button>
         </div>
         <div className="awayButtons">
           <button className="awayButtons__touchdown">Away Touchdown</button>
