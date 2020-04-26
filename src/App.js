52f8a62e6632e5d954c99360422750ae24420a19
Luke Adams <muhammadadams@gmail.com>
2020-04-26 19:57:02 -0400

    MVP

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 //TODO: STEP 1 - Import the useState hook.
-import React from "react";
+
 import "./App.css";
 import BottomRow from "./BottomRow";
+import React, { useState } from "react";
 
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
-
+  const [score, setScore] = useState(0);
+  const [point, setPoint] = useState(0);
+  //Lions
+  const touchDown = (e) => {
+    setScore(score + 6);
+  };
+  const fieldGoal = (e) => {
+    setScore(score + 1);
+  };
+  //Patriots
+  const pats = (e) => {
+    setPoint(point + 6);
+  };
+  const patsScore = (e) => {
+    setPoint(point + 1);
+  };
   return (
     <div className="container">
       <section className="scoreboard">
@@ -15,12 +31,12 @@ function App() {
 
             {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
 
-            <div className="home__score">32</div>
+            <div className="home__score">{score}</div>
           </div>
           <div className="timer">00:03</div>
           <div className="away">
-            <h2 className="away__name">Tigers</h2>
-            <div className="away__score">32</div>
+            <h2 className="away__name">Patriots</h2>
+            <div className="away__score">{point}</div>
           </div>
         </div>
         <BottomRow />
@@ -28,12 +44,20 @@ function App() {
       <section className="buttons">
         <div className="homeButtons">
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button className="homeButtons__touchdown">Home Touchdown</button>
-          <button className="homeButtons__fieldGoal">Home Field Goal</button>
+          <button className="homeButtons__touchdown" onClick={touchDown}>
+            Home Touchdown
+          </button>
+          <button className="homeButtons__fieldGoal" onClick={fieldGoal}>
+            Home Field Goal
+          </button>
         </div>
         <div className="awayButtons">
-          <button className="awayButtons__touchdown">Away Touchdown</button>
-          <button className="awayButtons__fieldGoal">Away Field Goal</button>
+          <button className="awayButtons__touchdown" onClick={pats}>
+            Away Touchdown
+          </button>
+          <button className="awayButtons__fieldGoal" onClick={patsScore}>
+            Away Field Goal
+          </button>
         </div>
       </section>
     </div>
