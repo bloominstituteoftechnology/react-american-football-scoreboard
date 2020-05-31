f8872f0a5098c2dde35dec365a32bd3dc2fcd465
Robel Mengistu <62121236+robelv2020@users.noreply.github.com>
2020-05-31 11:22:03 -0600

    added function to set scores using useState

--- src/App.js
+++ src/App.js
@@ -5,9 +5,26 @@ import "./App.css";
 
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
-  const [homeSc, setHomeSc] = useState(0)
-  const [awaySc, setAwaySc] = useState(0)
+  const [homeSco, setHomeSc] = useState(0) // home set to 0 for starting score
+  const [awaySco, setAwaySc] = useState(0) // away set to 0 for starting score
+
+  let homeTchDwn = el => {
+    setHomeSc(homeSco + 7)// home set to current score + 7 for touchdown
+  }
+
+  let AwayTchDwn = el => {
+    setAwaySc(awaySco + 7)// away set to current score + 7 for touchdown
+  }
+
+  let homeFldGol = el => {
+    setHomeSc(homeSco + 3)// home set to current score + 3 for Fild Goal
+  }
   
+  let fildFldGol = el => {
+    setAwaySc(awaySco + 3)// away set to current score + 3 for Fild Goal
+  }
+
+
 
   return (
     <div className="container">
@@ -17,12 +34,12 @@ function App() {
             <h2 className="home__name">Lions</h2>
 
             {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
-            <div className="home__score">32</div>
+  <div className="home__score">{homeSco}</div>
           </div>
           <div className="timer">00:03</div>
           <div className="away">
             <h2 className="away__name">Tigers</h2>
-            <div className="away__score">32</div>
+            <div className="away__score">{awaySco}</div>
           </div>
         </div>
         <BottomRow />
