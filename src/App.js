f27a93a19805c8011403ae90ceaf4afe2c047d25
Donavan Terranova <dterran2@outlook.com>
2020-06-17 17:41:25 -0600

    added buttons

--- src/App.js
+++ src/App.js
@@ -5,14 +5,15 @@ import "./App.css";
 
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
-    const number = 0;
-    const [lionValue, lionSetValue] = useState(number);
-    const [tigerValue, tigerSetValue] = useState(number);
-    // function touchDown(){
-
+    const homeScore = 0;
+    const awayScore = 0;
+    const [lionScore, lionAddedPoints] = useState(homeScore);
+    const [tigerScore, tigerAddedPoints] = useState(awayScore);
+    // function TouchDown(){
+        // return
     // };
-    // function fieldGoal(){
-
+    // function FieldGoal(){
+        // return
     // };
 
   return (
@@ -23,12 +24,12 @@ function App() {
             <h2 className="home__name">Lions</h2>
 
             {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
-            <div className="home__score">{lionValue}</div>
+            <div className="home__score">{lionScore}</div>
           </div>
           <div className="timer">00:03</div>
           <div className="away">
             <h2 className="away__name">Tigers</h2>
-              <div className="away__score">{tigerValue}</div>
+              <div className="away__score">{tigerScore}</div>
           </div>
         </div>
         <BottomRow />
@@ -37,12 +38,12 @@ function App() {
         <div className="homeButtons">
 
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button onClick={() => lionValue + 7} className="homeButtons__touchdown">Home Touchdown</button>
-          <button onClick={() => lionValue(lionSetValue + 3)} className="homeButtons__fieldGoal">Home Field Goal</button>
+          <button onClick={() => lionAddedPoints(lionScore + 7)} className="homeButtons__touchdown">Home Touchdown</button>
+          <button onClick={() => lionAddedPoints(lionScore + 3)} className="homeButtons__fieldGoal">Home Field Goal</button>
         </div>
         <div className="awayButtons">
-          <button onClick={() => tigerValue(tigerSetValue + 7)} className="awayButtons__touchdown">Away Touchdown</button>
-          <button onClick={() => tigerValue(tigerSetValue + 3)} className="awayButtons__fieldGoal">Away Field Goal</button>
+          <button onClick={() => tigerAddedPoints(tigerScore + 7)} className="awayButtons__touchdown">Away Touchdown</button>
+          <button onClick={() => tigerAddedPoints(lionScore + 3)} className="awayButtons__fieldGoal">Away Field Goal</button>
         </div>
       </section>
     </div>
