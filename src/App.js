b16a91a1bfb25d20dc330b9525984c99f5a94b0a
karmaeiic <46303520+karmaeiic@users.noreply.github.com>
2020-06-17 17:12:24 -0700

    added Onclicker and installed npm

--- src/App.js
+++ src/App.js
@@ -3,41 +3,60 @@ import React from "react";
 import BottomRow from "./BottomRow";
 import "./App.css";
 
+
 function App() {
-  //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
+    //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
 
-  return (
-    <div className="container">
-      <section className="scoreboard">
-        <div className="topRow">
-          <div className="home">
-            <h2 className="home__name">Lions</h2>
+    return ( < div className = "container" >
+        <
+        section className = "scoreboard" >
+        <
+        div className = "topRow" >
+        <
+        div className = "home" >
+        <
+        h2 className = "home__name" > Lions < /h2>
 
-            {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
-            <div className="home__score">32</div>
-          </div>
-          <div className="timer">00:03</div>
-          <div className="away">
-            <h2 className="away__name">Tigers</h2>
-            <div className="away__score">32</div>
-          </div>
-        </div>
-        <BottomRow />
-      </section>
-      <section className="buttons">
-        <div className="homeButtons">
+        { /* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */ } <
+        div className = "home__score" > < /div> <
+        /div> < /
+        div > <
+        div className = "timer" > 00: 03 < /div> <
+        div className = "away" >
+        <
+        h2 className = "away__name" > Tigers < /h2> <
+        div className = "away__score" > < /div> < /
+        div > <
+        /div> <
+        BottomRow / >
+        <
+        /section> <
+        section className = "buttons" >
+        <
+        div className = "homeButtons" >
 
-          {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button className="homeButtons__touchdown">Home Touchdown</button>
-          <button className="homeButtons__fieldGoal">Home Field Goal</button>
-        </div>
-        <div className="awayButtons">
-          <button className="awayButtons__touchdown">Away Touchdown</button>
-          <button className="awayButtons__fieldGoal">Away Field Goal</button>
-        </div>
-      </section>
-    </div>
-  );
+        { /* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */ } <
+        button className = "homeButtons__touchdown"
+        onClick = {
+            () => setCount(count + 6)
+        } > Home Touchdown < /button> <
+        button className = "homeButtons__fieldGoal"
+        onClick = {
+            () => setCount(count + 1)
+        } > Home Field Goal < /button> < /
+        div > <
+        div className = "awayButtons" >
+        <
+        button className = "awayButtons__touchdown" {
+            () => setCount(count + 6)
+        } > Away Touchdown < /button> <
+        button className = "awayButtons__fieldGoal" {
+            () => setCount(count + 1)
+        } > Away Field Goal < /button> < /
+        div > <
+        /section> < /
+        div >
+    );
 }
 
 export default App;
