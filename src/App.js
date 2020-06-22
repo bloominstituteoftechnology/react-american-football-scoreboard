eab012d9276ffc318179a6822e8c6e4e8a1439ed
unknown <clay2000lbs@gmail.com>
2020-06-21 19:39:11 -0500

    minor change

--- src/App.js
+++ src/App.js
@@ -39,7 +39,7 @@ function App() {
         </div>
         <div className="awayButtons">
           <button onClick={() => setAwayScore(awayScore + 7)} className="awayButtons__touchdown">Away Touchdown</button>
-          <button onClick={() => setAwayScore(awayScore + 7)} className="awayButtons__fieldGoal">Away Field Goal</button>
+          <button onClick={() => setAwayScore(awayScore + 3)} className="awayButtons__fieldGoal">Away Field Goal</button>
         </div>
       </section>
     </div>
