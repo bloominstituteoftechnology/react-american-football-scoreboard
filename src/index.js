fff484f7ce9fceda4d995649ed6cf42b445dd9e7
David Veele <you@example.com>
2020-07-24 23:15:25 -0400

    Debugging

--- src/index.js
+++ src/index.js
@@ -1,6 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+/** @format */
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+
+/*1. Hold each team's current score in a state value
+2. Render each team's current score that is in state to the DOM.
+3. Be able to click the different buttons to increment the appropriate team's score by the correct amount
+
+#### Adding Team Scores to the Component's State
+
+- [ ] Import the `useState` hook
+- [ ] Set up the state values for the Lions team score using the state hook
+
+```js
+const [value, setValue] = useState(); // Give these better names, and decide whether you want to pass an initial score into the state hook as the initialValue
+```
+
+- [ ] Set up the state value for the Tigers team score using a second state hook call*/
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, document.getElementById("root"));
